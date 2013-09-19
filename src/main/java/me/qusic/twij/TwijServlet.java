25cd4b011736dbdbaa902397844c2cd582e9421e
Dazzy Ding <h@dazzyd.org>
2013-09-19 15:34:37 +0800

    (Ignorable) Replace TAB with SPACE

--- src/main/java/me/qusic/twij/TwijServlet.java
+++ src/main/java/me/qusic/twij/TwijServlet.java
@@ -25,136 +25,136 @@ import org.scribe.oauth.OAuthService;
 
 public class TwijServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private final OAuthService service;
-	private final Token accessToken;
+    private final OAuthService service;
+    private final Token accessToken;
 
-	private final List<String> logs;
+    private final List<String> logs;
 
-	{
-		Map<String, String> environment = System.getenv();
-		String consumerKey = environment.get("ConsumerKey");
-		String consumerSecret = environment.get("ConsumerSecret");
-		String accessToken = environment.get("AccessToken");
-		String accessTokenSecret = environment.get("AccessTokenSecret");
-		this.service = new ServiceBuilder().provider(TwitterApi.class).apiKey(consumerKey).apiSecret(consumerSecret).build();
-		this.accessToken = new Token(accessToken, accessTokenSecret);
-		this.logs = new ArrayList<String>();
-	}
+    {
+        Map<String, String> environment = System.getenv();
+        String consumerKey = environment.get("ConsumerKey");
+        String consumerSecret = environment.get("ConsumerSecret");
+        String accessToken = environment.get("AccessToken");
+        String accessTokenSecret = environment.get("AccessTokenSecret");
+        this.service = new ServiceBuilder().provider(TwitterApi.class).apiKey(consumerKey).apiSecret(consumerSecret).build();
+        this.accessToken = new Token(accessToken, accessTokenSecret);
+        this.logs = new ArrayList<String>();
+    }
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (twij(request, response)) {
-			return;
-		} else {
-			processRequest(generateRequest(request), response);
-		}
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (twij(request, response)) {
+            return;
+        } else {
+            processRequest(generateRequest(request), response);
+        }
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (twij(request, response)) {
-			return;
-		} else {
-			processRequest(generateRequest(request), response);
-		}
-	}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (twij(request, response)) {
+            return;
+        } else {
+            processRequest(generateRequest(request), response);
+        }
+    }
 
-	private OAuthRequest generateRequest(HttpServletRequest request) throws IOException {
-		String url;
-		boolean streaming;
-		String method = request.getMethod();
-		String contentType = request.getContentType();
-		boolean includesQuery = !(method.equals("POST") && (contentType == null || contentType.equals("application/x-www-form-urlencoded")));
+    private OAuthRequest generateRequest(HttpServletRequest request) throws IOException {
+        String url;
+        boolean streaming;
+        String method = request.getMethod();
+        String contentType = request.getContentType();
+        boolean includesQuery = !(method.equals("POST") && (contentType == null || contentType.equals("application/x-www-form-urlencoded")));
 
-		String host = "api.twitter.com";
-		String path = request.getPathInfo();
-		if (path.startsWith("/1.1/statuses/filter") || path.startsWith("/1.1/statuses/sample") || path.startsWith("/1.1/statuses/firehose")) {
-			streaming = true;
-			host = "stream.twitter.com";
-		} else if (path.startsWith("/1.1/user") && !path.startsWith("/1.1/users")) {
-			streaming = true;
-			host = "userstream.twitter.com";
-		} else if (path.startsWith("/1.1/site")) {
-			streaming = true;
-			host = "sitestream.twitter.com";
-		} else {
-			streaming = false;
-		}
-		url = "https://" + host + path;
-		String query = request.getQueryString();
-		if (includesQuery && query != null) {
-			url += "?" + query;
-		}
+        String host = "api.twitter.com";
+        String path = request.getPathInfo();
+        if (path.startsWith("/1.1/statuses/filter") || path.startsWith("/1.1/statuses/sample") || path.startsWith("/1.1/statuses/firehose")) {
+            streaming = true;
+            host = "stream.twitter.com";
+        } else if (path.startsWith("/1.1/user") && !path.startsWith("/1.1/users")) {
+            streaming = true;
+            host = "userstream.twitter.com";
+        } else if (path.startsWith("/1.1/site")) {
+            streaming = true;
+            host = "sitestream.twitter.com";
+        } else {
+            streaming = false;
+        }
+        url = "https://" + host + path;
+        String query = request.getQueryString();
+        if (includesQuery && query != null) {
+            url += "?" + query;
+        }
 
-		OAuthRequest oauthRequest = null;
-		if (method.equals("GET")) {
-			oauthRequest = new OAuthRequest(Verb.GET, url);
-		} else if (method.equals("POST")) {
-			oauthRequest = new OAuthRequest(Verb.POST, url);
-			if (includesQuery) {
-				InputStream in = request.getInputStream();
-				ByteArrayOutputStream out = new ByteArrayOutputStream();
-				byte[] buffer = new byte[1024];
-				int count = 0;
-				while ((count = in.read(buffer)) != -1) {
-					out.write(buffer, 0, count);
-				}
-				byte[] body = out.toByteArray();
-				in.close();
-				out.close();
-				oauthRequest.addPayload(body);
-				oauthRequest.addHeader("Content-Type", contentType);
-			} else {
-				@SuppressWarnings("unchecked")
-				Enumeration<String> enumeration = request.getParameterNames();
-				while (enumeration.hasMoreElements()) {
-					String name = enumeration.nextElement();
-					oauthRequest.addBodyParameter(name, request.getParameter(name));
-				}
-			}
-		}
-		oauthRequest.setConnectionKeepAlive(streaming);
+        OAuthRequest oauthRequest = null;
+        if (method.equals("GET")) {
+            oauthRequest = new OAuthRequest(Verb.GET, url);
+        } else if (method.equals("POST")) {
+            oauthRequest = new OAuthRequest(Verb.POST, url);
+            if (includesQuery) {
+                InputStream in = request.getInputStream();
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int count = 0;
+                while ((count = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, count);
+                }
+                byte[] body = out.toByteArray();
+                in.close();
+                out.close();
+                oauthRequest.addPayload(body);
+                oauthRequest.addHeader("Content-Type", contentType);
+            } else {
+                @SuppressWarnings("unchecked")
+                Enumeration<String> enumeration = request.getParameterNames();
+                while (enumeration.hasMoreElements()) {
+                    String name = enumeration.nextElement();
+                    oauthRequest.addBodyParameter(name, request.getParameter(name));
+                }
+            }
+        }
+        oauthRequest.setConnectionKeepAlive(streaming);
 
-		return oauthRequest;
-	}
+        return oauthRequest;
+    }
 
-	private void processRequest(OAuthRequest oauthRequest, HttpServletResponse response) throws IOException {
-		service.signRequest(accessToken, oauthRequest);
-		Response oauthResponse = oauthRequest.send();
+    private void processRequest(OAuthRequest oauthRequest, HttpServletResponse response) throws IOException {
+        service.signRequest(accessToken, oauthRequest);
+        Response oauthResponse = oauthRequest.send();
 
-		response.setContentType(oauthResponse.getHeader("Content-Type"));
-		response.setStatus(oauthResponse.getCode());
-		response.setBufferSize(0);
+        response.setContentType(oauthResponse.getHeader("Content-Type"));
+        response.setStatus(oauthResponse.getCode());
+        response.setBufferSize(0);
 
-		InputStream in = oauthResponse.getStream();
-		OutputStream out = response.getOutputStream();
-		byte[] buffer = new byte[1024];
-		int count = 0;
-		while ((count = in.read(buffer)) != -1) {
-			out.write(buffer, 0, count);
-		}
-		in.close();
-		out.close();
-	}
+        InputStream in = oauthResponse.getStream();
+        OutputStream out = response.getOutputStream();
+        byte[] buffer = new byte[1024];
+        int count = 0;
+        while ((count = in.read(buffer)) != -1) {
+            out.write(buffer, 0, count);
+        }
+        in.close();
+        out.close();
+    }
 
-	private boolean twij(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		String path = request.getPathInfo();
-		if (path.equals("/")) {
-			response.sendRedirect("http://www.google.com/ncr");
-			return true;
-		} else if (path.equals("/logs")) {
-			response.setContentType("text/plain");
-			response.setStatus(200);
-			PrintWriter writer = response.getWriter();
-			for (String log : logs) {
-				writer.println(log);
-			}
-			writer.close();
-			return true;
-		} else {
-			return false;
-		}
-	}
+    private boolean twij(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String path = request.getPathInfo();
+        if (path.equals("/")) {
+            response.sendRedirect("http://www.google.com/ncr");
+            return true;
+        } else if (path.equals("/logs")) {
+            response.setContentType("text/plain");
+            response.setStatus(200);
+            PrintWriter writer = response.getWriter();
+            for (String log : logs) {
+                writer.println(log);
+            }
+            writer.close();
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
